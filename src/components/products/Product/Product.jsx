3f13ac0b9c37f60f42d1935c132a20adb81e839d
Artur Konopka <artur.konopka.1994@gmail.com>
2021-08-01 10:28:15 +0100

    Started E-commerce shop react project

---
+++ src/components/products/Product/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+import { classes } from 'istanbul-lib-coverage';
+
+const Product = () => {
+  return (
+    <Card className={classes.root}>
+      
+    </Card>
+  )
+}
+
+export default Product;
